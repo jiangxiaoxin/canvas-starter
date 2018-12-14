0f22b45985b5d7c083fbfce69196fa0f7c8e8890
jiangxiaoxin <jiangyanxin1989@gmail.com>
2018-12-14 17:45:30 +0800

    update

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+let bodyWidth = document.body.clientWidth
+let bodyHeight = document.body.clientHeight
+
+let canvas = <HTMLCanvasElement>document.getElementById('playground')
+canvas.width = bodyWidth
+canvas.height = bodyHeight
